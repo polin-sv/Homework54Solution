e0c9da27244cd9e3cb459fd3d8373246d9e35267
polin-sv <polinasvetlay@icloud.com>
2026-04-12 16:06:11 +0300

    soft task02

--- task02/logic.h
+++ task02/logic.h
@@ -1,4 +1,4 @@
 #include <iostream>
 using namespace std;
 
-int tribonacci_number(int number);
+int tribonacci_number(int index);
---
+++ task03/logic.h
@@ -0,0 +1,4 @@
+#include <iostream>
+using namespace std;
+
+int binary_search(int number);
