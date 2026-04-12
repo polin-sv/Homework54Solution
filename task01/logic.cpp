ab6c2a57625710b59234a699d13a2c371efc4fc8
polin-sv <polinasvetlay@icloud.com>
2026-04-12 15:45:30 +0300

    soft task01

--- task01/logic.cpp
+++ task01/logic.cpp
@@ -1,2 +1,12 @@
 #include "logic.h"
 
+bool is_power_of_four(int number) {
+	if (number <= 0) {
+		return false;
+	}
+	if (number == 1) {
+		return true;
+	}
+
+	return is_power_of_four(number / 4);
+}
